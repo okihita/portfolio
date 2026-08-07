cb83fe02d07664ab0558f4b77535559d13b21acd
Okihita Sihaloho <okihita@gmail.com>
2026-08-07 18:28:57 +0700

    style(erlangga): set subsidiary image container background to warm Risograph soy-paper tone (#f6f4ee)

--- components/erlangga/ErlanggaSubsidiariesView.tsx
+++ components/erlangga/ErlanggaSubsidiariesView.tsx
@@ -498,8 +498,8 @@ export default function ErlanggaSubsidiariesView() {
                   key={sub.id}
                   className="rounded-2xl border border-zinc-200/80 dark:border-zinc-800 bg-white dark:bg-zinc-900/60 overflow-hidden shadow-xs flex flex-col lg:flex-row items-stretch"
                 >
-                  {/* LEFT IMAGE CONTAINER (Zero Text Cropping with object-contain) */}
-                  <div className="relative w-full lg:w-[320px] aspect-[3/4] bg-slate-950 shrink-0 overflow-hidden flex items-center justify-center p-2 border-b lg:border-b-0 lg:border-r border-zinc-200/80 dark:border-zinc-800">
+                  {/* LEFT IMAGE CONTAINER (Seamless Risograph Soy-Paper Background) */}
+                  <div className="relative w-full lg:w-[320px] aspect-[3/4] bg-[#f6f4ee] dark:bg-[#111827] shrink-0 overflow-hidden flex items-center justify-center p-2 border-b lg:border-b-0 lg:border-r border-zinc-200/80 dark:border-zinc-800">
                     <img
                       src={sub.image}
                       alt={sub.name}
